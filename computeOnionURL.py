53d8cd233f44e31ba1126d991265e2150dd0507b
User <Willspri@gmail.com>
2025-11-18 19:01:26 +1300

    update

--- computeOnionURL.py
+++ computeOnionURL.py
@@ -1,10 +1,20 @@
 import base64
 import hashlib
 
-pubkey = open("hs_ed25519_public_key", "rb").read()[32:] # : skips first 32 bytes 
+
+pubkey = bytes.fromhex(
+    "1d0f172c16421e2cd26ae3c98e0f5d2a"
+    "f8f2a7b3e9dc10d3e2efa69f98f7b2cd"
+)
+
+#pubkey = open("hs_ed25519_public_key", "rb").read()[32:] # : skips first 32 bytes 
 print(pubkey)
+#tor version 3
 version = bytes([0x03])
+print(version)
+#checksum computation
 checksum_input = b".onion checksum" + pubkey + version
+print(checksum_input)
 
 checksum = hashlib.sha3_256(checksum_input).digest()[:2]
 
